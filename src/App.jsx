07944ddfef5a8f500e6f8fc6f5e0876b7bd14a40
Hemanshu Thakar <hemanshu.vibrantindiatech@gmail.com>
2026-02-06 18:25:12 +0530

    first commit 123456

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
